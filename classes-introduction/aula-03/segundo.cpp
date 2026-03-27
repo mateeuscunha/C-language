45aaae0d74953d365b8868b35895e17c89353e69
Mateus Cunha <mateuscunhazp@gmail.com>
2026-03-27 08:06:33 -0300

    variables

---
+++ classes-introduction/aula-03/segundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int inteiro = 5;
+    cout << inteiro << endl;
+
+    float real = 5.2;
+    cout << real << endl;
+
+
+    return 0;
+}
